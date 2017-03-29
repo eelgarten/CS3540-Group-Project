7775219f6af7fc368049c7e3a4823a8d0f71c4b1
eelgarten <elgarten.e@husky.neu.edu>
2017-03-29 17:01:03 -0400

    Initial commit
    Signed-off-by: eelgarten <elgarten.e@husky.neu.edu>

---
+++ Assets/Scripts/BulletMoveForward.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMoveForward : MonoBehaviour {
+
+	public float maxSpeed = 8;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 pos = transform.position;
+
+		Vector3 velocity = new Vector3 (0, maxSpeed * Time.deltaTime, 0);
+
+		pos += transform.rotation * velocity;
+
+		transform.position = pos;
+		
+	}
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+
+	public float fireDelay = 0.25f;
+	float coolDownTimer = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		coolDownTimer -= Time.deltaTime;
+
+		if (Input.GetButton ("Fire1") && coolDownTimer <= 0) {
+			coolDownTimer = fireDelay;
+
+			Vector3 offset = transform.rotation * new Vector3 (0, 0.5f, 0);
+
+			Instantiate (bulletPrefab, transform.position + offset, transform.rotation);
+
+		}
+	}
+}
